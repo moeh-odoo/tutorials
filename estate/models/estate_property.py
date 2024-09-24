b7e2d208e39e0f84e4904639bda64a56021d0711
moeh <moeh@odoo.com>
2024-09-24 17:56:25 +0400

    Create new app

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,15 +7,22 @@ class estateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Char()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(copy=False,default=fields.Date.add(fields.Date.today(), months=3))
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
     garden = fields.Boolean()
     garden_area = fields.Integer()
+    Active = fields.Boolean(default=True)
     garden_orientation = fields.Selection(
         selection=[('North', 'north'), ('South', 'south'), ('East','east'), ('West','west')]
+    )
+    state = fields.Selection(
+        selection = [('New','New'), ('Received','Received'), ('Accepted','Accepted'), ('Sold','Sold'), ('Canceled','Canceled')],
+        required = True,
+        copy = False,
+        default = 'New'
     )
