1ee3938521b492a258d445e078f924f263d997a0
moeh <moeh@odoo.com>
2024-09-26 12:11:31 +0400

    Ch8

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,4 +1,4 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 class estateProperty(models.Model):
     _name = "estate_property"
@@ -21,7 +21,35 @@ class estateProperty(models.Model):
     buyer_id = fields.Many2one("res.partner", string="Buyer", copy=False)
     sales_person_id = fields.Many2one("res.users", string="Sales")
     estate_property_tag_id = fields.Many2many("estate_property_tag", string="Property Tags")
-    offer_ids = fields.One2many("estate_property_offer","property_id",string="offer")
+    offer_ids = fields.One2many("estate_property_offer","property_id",string="offer") # [21,23,24].mapped
+    total_area = fields.Float(compute="_compute_total")
+
+    @api.depends("garden_area","living_area")
+    def _compute_total(self):
+        for record in self:
+            record.total_area = record.garden_area + record.living_area;
+
+    best_price = fields.Float(compute="_compute_best_price")
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        print('hello')
+        for record in self:
+            print(record)
+            if record.offer_ids:
+                record.best_price = max(record.offer_ids.mapped('price'))
+            else:
+                record.best_price = 0.0
+    
+    @api.onchange('garden')
+    def _onchange_garden(self):
+        if self.garden: 
+            self.garden_area =10
+            self.garden_orientation = 'North'
+        else:
+            self.garden_area = 0
+            self.garden_orientation = False
+            
     garden_orientation = fields.Selection(
         selection=[('North', 'north'), ('South', 'south'), ('East','east'), ('West','west')]
     )
--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,5 +1,5 @@
-from odoo import fields, models
-
+from odoo import api, fields, models
+from datetime import date
 class estatePropertyOffer(models.Model):
     _name = "estate_property_offer"
     _description = "Estate Property Offer"
@@ -8,5 +8,24 @@ class estatePropertyOffer(models.Model):
         selection=[('Accepted', 'Accepted'), ('Refused', 'Refused')],
         copy = False
     )
+    validity = fields.Integer(default=7)
+    date_deadline = fields.Date(compute = "_compute_date_deadline", inverse = "_inverse_date_deadline")
+    #fields.Date.add(fields.Date.today(), months=3)
+    @api.depends('validity')
+    def _compute_date_deadline(self):
+        for record in self:
+                record.date_deadline = fields.Date.add(fields.Date.today(), days=record.validity)
+
+    def _inverse_date_deadline(self):
+        for record in self:
+            if record.date_deadline:
+                # Calculate the difference between date_deadline and today's date
+                today = fields.Date.today()  # Get today's date
+                deadline = fields.Date.from_string(record.date_deadline)  # Convert deadline to date object
+                days_difference = (deadline - date.today()).days  # Subtract today's date from deadline
+                
+                # Assign the difference in days to the 'validity' field
+                record.validity = days_difference
+
     partner_id = fields.Many2one("res.partner", required = True)
     property_id = fields.Many2one("estate_property",required = True)
